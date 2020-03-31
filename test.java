73b03d85c62db94ee0ed3504bac0b0ce24f347e6
champ2025 <55731727+champ2025@users.noreply.github.com>
2020-03-31 11:27:12 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,37 +1,24 @@
 class Solution {
-  public int leastCoins(int[] coins, int amount) {
-    // This table will store the answer to our sub problems
-    int[] dp = new int[amount + 1];
-
-    // Initialize the dp table
-    Arrays.fill(dp, amount + 1);  
-
-    /*
-      The answer to making change with minimum coins for 0
-      will always be 0 coins no matter what the coins we are
-      given are
-    */
-    dp[0] = 0;
-
-    // Solve every subproblem from 1 to amount
-    for (int i = 1; i <= amount; i++) {
-      // For each coin we are given
-      for (int j = 0; j < coins.length; j++) {
-        // If it is less than or equal to the sub problem amount
-        if (coins[j] <= i) {
-          // Try it. See if it gives us a more optimal solution
-          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-        }
-      }
+    public int maxProfit(int[] prices) {
+        return calculate(prices, 0);
     }
 
-    /*
-      dp[amount] has our answer. If we do not have an answer then dp[amount]
-      will be amount + 1 and hence dp[amount] > amount will be true. We then
-      return -1.
-
-      Otherwise, dp[amount] holds the answer
-    */
-    return dp[amount] > amount ? -1 : dp[amount];
-  }
+    public int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
+    }
 }
